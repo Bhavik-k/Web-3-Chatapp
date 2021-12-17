2a4cd5db37998605b132a576694a91fb34136b3d
Bhavik Khawale <62886971+Bhavik-k@users.noreply.github.com>
2021-12-17 00:45:27 -0500

    added purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layout/**/*.{js,ts,jsx,tsx}',
+    './helpers/**/*.{js,ts,jsx,tsx}',
+    // Add more here
+],
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
